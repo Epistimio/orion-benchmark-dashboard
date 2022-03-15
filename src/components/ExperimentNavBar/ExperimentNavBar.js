9fa945f04e3c8306fca9074e95dbe1b2967f19cc
notoraptor <stevenbocco@gmail.com>
2022-03-15 12:06:44 -0400

    feat: search benchmarks, select benchmark and display algos and tasks

--- src/components/ExperimentNavBar/ExperimentNavBar.js
+++ src/components/ExperimentNavBar/ExperimentNavBar.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import { CloseFilled16 } from '@carbon/icons-react';
 import { Backend } from '../../utils/queryServer';
 import { BackendContext } from '../../BackendContext';
-
 import {
   SideNav,
+  Checkbox,
   StructuredListWrapper,
   StructuredListHead,
   StructuredListBody,
   StructuredListRow,
-  StructuredListInput,
   StructuredListCell,
-  Search,
+  ComboBox,
 } from 'carbon-components-react';
 
-import { settings } from 'carbon-components';
-
-const { prefix } = settings;
+function getAlgorithmName(algoDef) {
+  if (algoDef.hasOwnProperty('algorithm')) return algoDef.algorithm;
+  const keys = Object.keys(algoDef);
+  if (keys.length === 1) return keys[0];
+  return algoDef;
+}
 
 export class ExperimentNavBar extends React.Component {
   static contextType = BackendContext;
@@ -25,143 +25,133 @@ export class ExperimentNavBar extends React.Component {
     // prop: onSelectExperiment: function(experiment)
     super(props);
     this.state = {
-      experiments: null,
-      search: '',
+      benchmarks: null,
+      benchmark: null,
+      algorithms: new Set(),
+      tasks: new Set(),
     };
-    this.onSearch = this.onSearch.bind(this);
-    this.onUnselect = this.onUnselect.bind(this);
+    this.onChangeComboBox = this.onChangeComboBox.bind(this);
+    this.onSelectAlgo = this.onSelectAlgo.bind(this);
+    this.onSelectTask = this.onSelectTask.bind(this);
   }
   render() {
-    return (
+    return this.state.benchmarks === null ? (
+      ''
+    ) : (
       <SideNav
         isFixedNav
         expanded={true}
         isChildOfHeader={false}
         aria-label="Side navigation">
-        <StructuredListWrapper selection>
-          <StructuredListHead>
-            <StructuredListRow head>
-              <StructuredListCell head>{''}</StructuredListCell>
-              <StructuredListCell head>Experiment</StructuredListCell>
-              <StructuredListCell head>Status</StructuredListCell>
-            </StructuredListRow>
-          </StructuredListHead>
-          <StructuredListBody>
-            {this.renderExperimentsList()}
-          </StructuredListBody>
-        </StructuredListWrapper>
-        <Search
-          placeholder="Search experiment"
-          labelText="Search experiment"
-          onChange={this.onSearch}
+        <ComboBox
+          onChange={this.onChangeComboBox}
+          id={'combobox-benchmark'}
+          items={this.state.benchmarks}
+          itemToString={item => (item === null ? null : item.name)}
+          placeholder={'Search a benchmark ...'}
         />
+        {this.state.benchmark === null ? (
+          ''
+        ) : (
+          <StructuredListWrapper>
+            <StructuredListHead>
+              <StructuredListRow head>
+                <StructuredListCell head>Algorithms</StructuredListCell>
+                <StructuredListCell head>Tasks</StructuredListCell>
+              </StructuredListRow>
+            </StructuredListHead>
+            <StructuredListBody>
+              {this.renderBenchmarkSettings()}
+            </StructuredListBody>
+          </StructuredListWrapper>
+        )}
       </SideNav>
     );
   }
-  renderExperimentsList() {
-    if (this.state.experiments === null)
-      return this.renderMessageRow('Loading experiments ...');
-    if (!this.state.experiments.length)
-      return this.renderMessageRow('No experiment available');
-    // Apply search.
-    let experiments;
-    if (this.state.search.length) {
-      // String to search
-      experiments = this.state.experiments.filter(
-        experiment => experiment.toLowerCase().indexOf(this.state.search) >= 0
-      );
-      if (!experiments.length)
-        return this.renderMessageRow('No matching experiment');
-    } else {
-      // No string to search, display all experiments
-      experiments = this.state.experiments;
+  renderBenchmarkSettings() {
+    const benchmark = this.state.benchmark;
+    const algorithms = benchmark.algorithms.map(algo => getAlgorithmName(algo));
+    const tasks = Object.keys(benchmark.tasks);
+    algorithms.sort();
+    tasks.sort();
+    const nbRows = Math.max(algorithms.length, tasks.length);
+    const rows = [];
+    for (let i = 0; i < nbRows; ++i) {
+      rows.push({
+        algorithm: i < algorithms.length ? algorithms[i] : null,
+        task: i < tasks.length ? tasks[i] : null,
+      });
     }
-    return experiments.map((experiment, i) => (
-      <StructuredListRow label key={`row-${i}`}>
-        <StructuredListInput
-          id={`select-experiment-${i}`}
-          value={`row-${i}`}
-          title={`row-${i}`}
-          name="select-experiment"
-          onChange={() => this.props.onSelectExperiment(experiment)}
-        />
-        <StructuredListCell>
-          <span
-            title={`unselect experiment '${experiment}'`}
-            style={{
-              visibility:
-                this.context.experiment === experiment ? 'visible' : 'hidden',
-            }}
-            onClick={event =>
-              this.onUnselect(event, experiment, `select-experiment-${i}`)
-            }>
-            <CloseFilled16
-              className={`${prefix}--structured-list-svg`}
-              aria-label="unselect experiment">
-              <title>unselect experiment</title>
-            </CloseFilled16>
-          </span>
-        </StructuredListCell>
-        <StructuredListCell>
-          [{i + 1}] {experiment}
-        </StructuredListCell>
-        <StructuredListCell>
-          <ProgressBar>
-            <ProgressBar variant="success" now={35} key={1} />
-            <ProgressBar variant="warning" now={20} key={2} />
-            <ProgressBar variant="danger" now={10} key={3} />
-            <ProgressBar variant="info" now={15} key={4} />
-          </ProgressBar>
-        </StructuredListCell>
+    return rows.map((row, i) => (
+      <StructuredListRow key={i}>
+        {row.algorithm === null ? (
+          <StructuredListCell />
+        ) : (
+          <StructuredListCell>
+            <Checkbox
+              labelText={row.algorithm}
+              id={`algorithm-${i}`}
+              checked={this.state.algorithms.has(row.algorithm)}
+              onChange={(checked, id, event) =>
+                this.onSelectAlgo(row.algorithm, checked)
+              }
+            />
+          </StructuredListCell>
+        )}
+        {row.task === null ? (
+          <StructuredListCell />
+        ) : (
+          <StructuredListCell>
+            <Checkbox
+              labelText={row.task}
+              id={`task-${i}`}
+              checked={this.state.tasks.has(row.task)}
+              onChange={(checked, id, event) =>
+                this.onSelectTask(row.task, checked)
+              }
+            />
+          </StructuredListCell>
+        )}
       </StructuredListRow>
     ));
-  }
-  renderMessageRow(message) {
-    return (
-      <StructuredListRow>
-        <StructuredListCell />
-        <StructuredListCell>{message}</StructuredListCell>
-        <StructuredListCell />
-      </StructuredListRow>
-    );
   }
   componentDidMount() {
     const backend = new Backend(this.context.address);
     backend
-      .query('experiments')
-      .then(results => {
-        const experiments = results.map(experiment => experiment.name);
-        experiments.sort();
-        this.setState({ experiments });
+      .query('benchmarks')
+      .then(benchmarks => {
+        this.setState({ benchmarks });
       })
       .catch(error => {
-        this.setState({ experiments: [] });
+        this.setState({ benchmarks: [] });
       });
   }
-  onSearch(event) {
-    this.setState({ search: event.target.value.toLowerCase() });
+  onChangeComboBox(event) {
+    const benchmark = event.selectedItem;
+    if (benchmark === null) {
+      this.setState({ benchmark, algorithms: new Set(), tasks: new Set() });
+    } else {
+      const algorithms = benchmark.algorithms.map(algo =>
+        getAlgorithmName(algo)
+      );
+      this.setState({
+        benchmark,
+        algorithms: new Set(algorithms),
+        tasks: new Set(Object.keys(benchmark.tasks)),
+      });
+    }
   }
-  onUnselect(event, experiment, inputID) {
-    /*
-     * By default, a click anywhere in experiment line (including on cross icon)
-     * will select the experiment.
-     * Here, we want to click on cross icon to deselect experiment.
-     * So, we must prevent click on cross icon to act like click on experiment
-     * line.
-     * To do that, we call event's preventDefault() method to cancel default
-     * behavior.
-     * */
-    event.preventDefault();
-    /*
-     * Uncheck hidden radiobutton input associated to selected experiment.
-     * This is necessary to allow to immediately click again to experiment
-     * line to re-select experiment.
-     * */
-    document.getElementById(inputID).checked = false;
-    /*
-     * Tell global interface to unselect experiment
-     * */
-    this.props.onSelectExperiment(null);
+  onSelectAlgo(algorithm, checked) {
+    const algorithms = new Set(this.state.algorithms);
+    if (checked) algorithms.add(algorithm);
+    else algorithms.delete(algorithm);
+    this.setState({ algorithms });
+  }
+  onSelectTask(task, checked) {
+    const tasks = new Set(this.state.tasks);
+    if (checked) tasks.add(task);
+    else tasks.delete(task);
+    this.setState({ tasks });
   }
 }
 
